55a5b95fcac295c48f2aef3d60b7d1dfdb4cb267
george2 <hyped@teamliquid.com>
2022-02-15 16:49:21 -0800

    Adjust green, add underline class for regular text

--- src/components/Method/Pomodoro/Pomodoro.js
+++ src/components/Method/Pomodoro/Pomodoro.js
@@ -17,8 +17,8 @@ function Pomodoro(props) {
             </img>
             <ul className='card-ul'>
             <li className='card-li'>Commit to working on a task for a set period of time, then take a break!</li>
-            <li className='card-li'>Focus on process not product, i.e. "I will put 25 minutes of work into this
-                 task", not "I will work on this task until complettion".</li>
+            <li className='card-li'>Focus on <span className='underline green-underline'>process not product</span> , i.e. "I will put 25 minutes of work into this
+                 task", rather than "I will complete this task".</li>
             </ul>
             <ButtonWrapper>
                 <button className='green-button'> 10:00   🍅</button>
